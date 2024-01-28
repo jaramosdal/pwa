36b70ee8ab5b3b6d0eb532daa535c4461843fa7a
Javier Ramos Nodal <javiramosnodal@gmail.com>
2024-01-28 09:42:02 +0100

    Web App Setup and Structure.

---
+++ car-deals-2/js/app.js
@@ -0,0 +1,9 @@
+import { loadCarPage } from "./carPageService.js";
+import { loadCars } from "./carService.js";
+// import swRegister from "./swRegister.js";
+
+window.pageEvents = {
+  loadCarPage,
+};
+
+loadCars();
