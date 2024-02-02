a8272c45a6c6836145352b83972af915849fad9a
Javier Ramos Nodal <javiramos@soliss.com>
2024-02-02 14:39:59 +0100

    Retreiving data.

--- car-deals-2/js/carService.js
+++ car-deals-2/js/carService.js
@@ -1,8 +1,10 @@
 import { API_URL_LATEST } from "./constants.js";
 import { appendCars } from "./template.js";
+import { addCars } from "./clientStorage.js";
 
 export const loadCars = async () => {
   const response = await fetch(API_URL_LATEST);
   const data = await response.json();
   appendCars(data.cars);
+  await addCars(data.cars);
 };
