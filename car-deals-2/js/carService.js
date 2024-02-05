4e858e5bf31065d84b7a404fa9ac35f2f90ce7b0
Javier Ramos Nodal <javiramos@soliss.com>
2024-02-05 14:34:53 +0100

    cache.

--- car-deals-2/js/carService.js
+++ car-deals-2/js/carService.js
@@ -1,10 +1,10 @@
+import { preCacheDetailsPage } from "./carPageService.js";
 import { API_URL_LATEST } from "./constants.js";
 import { appendCars } from "./template.js";
 import { addCars, getCars, getLastItemId } from "./clientStorage.js";
 
 export const loadCars = async () => {
-  // Obtiene 3 coches vía API y los carga en BBDD local
-  await loadCarsRequest();
+  document.getElementById("connection-status").innerHTML = await fetchPromise();
 
   // Carga los 3 primeros coches de la BBDD local
   const cars = await getCars();
@@ -14,6 +14,26 @@ export const loadCars = async () => {
   appendCars(cars);
 };
 
+const fetchPromise = () => {
+  const promiseRequest = new Promise(async (resolve) => {
+    try {
+      await loadCarsRequest();
+    } catch (error) {
+      resolve("Mo connection, showing offline results");
+    }
+    resolve("This connection is OK, showing latest results");
+  });
+  const promiseHanging = new Promise((resolve) => {
+    setTimeout(
+      resolve,
+      3000,
+      "The connection is hanging, showing offline results"
+    );
+  });
+  return Promise.race([promiseRequest, promiseHanging]);
+};
+
+// Obtiene 3 coches vía API y los carga en BBDD local
 export const loadCarsRequest = async () => {
   // Obtiene 3 coches a partir del indicado
   const requestURL = `${API_URL_LATEST}?carId=${await getLastItemId()}`;
@@ -22,4 +42,5 @@ export const loadCarsRequest = async () => {
 
   // Añade los 3 coches a la BBDD local
   await addCars(data.cars);
+  data.cars.forEach(preCacheDetailsPage);
 };
