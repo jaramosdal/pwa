935b7684d425f33d9b520f14e18aeeb5947b242f
Javier Ramos Nodal <javiramos@soliss.com>
2024-02-09 14:07:35 +0100

    Network First Then Offline Strategy.

--- car-deals-2/js/swRegister.js
+++ car-deals-2/js/swRegister.js
@@ -21,4 +21,25 @@ export default async () => {
   serviceWorker.addEventListener("statechange", (e) => {
     console.log(e.target.state);
   });
+
+  // Evento lanzado cuando Nueva versión de SW encontrada
+  swRegistration.addEventListener("updatefound", () => {
+    swRegistration.installing.addEventListener("statechange", (e) => {
+      console.log("New service worker state: ", e.target.state);
+    });
+    console.log("New service worker found!: ", swRegistration);
+  });
+
+  // An extra event that is fired when the service worker controlling this page changes
+  // through the self.skipWaiting()
+  navigator.serviceWorker.addEventListener("controllerchange", () => {
+    console.log("Controller Changed!");
+  });
+
+  // Se comprueba si hay una nueva versión del SW cada vez que se navega a una página que forme parte de su scope o con cada fetch
+  // Pero también podemos comprobar manualmente si hay una nueva versión de la siguiente manera:
+  setInterval(() => {
+    console.log("Comprobando nueva versión de SW.");
+    swRegistration.update();
+  }, 1000 * 5);
 };
--- car-deals-2/sw.js
+++ car-deals-2/sw.js
@@ -1,6 +1,14 @@
 "use strict";
+// versión 1.0
 
-const carDealsCacheName = "carDealsCacheV1";
+const carDealsCacheName = "carDealsCacheV3";
+const carDealsCacheImagesName = "carDealsCacheImagesV1";
+const carDealsCachePagesName = "carDealsCachePagesV1";
+const carDealsCache = [
+  carDealsCacheName,
+  carDealsCacheImagesName,
+  carDealsCachePagesName,
+];
 const carDealsCacheFiles = [
   "https://cdn.jsdelivr.net/npm/pwacompat@2.0.17/pwacompat.min.js",
   "https://cdn.jsdelivr.net/gh/bstavroulakis/progressive-web-apps/resources/localforage.js",
@@ -16,9 +24,16 @@ const carDealsCacheFiles = [
   "style.css",
 ];
 
+const latestsPath =
+  "/pluralsight/courses/progressive-web-apps/service/latest-deals.php";
+const imagePath =
+  "/pluralsight/courses/progressive-web-apps/service/car-image.php";
+const carPath = "/pluralsight/courses/progressive-web-apps/service/car.php";
+
 // Estado Installing
 self.addEventListener("install", (event) => {
   console.log("From SW: Install Event", event);
+  self.skipWaiting();
   const preCache = async () => {
     const cache = await caches.open(carDealsCacheName);
     return cache.addAll(carDealsCacheFiles);
@@ -28,4 +43,59 @@ self.addEventListener("install", (event) => {
 
 self.addEventListener("activate", (event) => {
   console.log("From SW: Activate Event", event);
+  self.clients.claim();
+  const clearCache = async () => {
+    const keys = await caches.keys();
+    keys.forEach(async (k) => {
+      if (carDealsCache.includes(k)) {
+        return;
+      }
+      await caches.delete(k);
+    });
+  };
+  event.waitUntil(clearCache());
 });
+
+self.addEventListener("fetch", (event) => {
+  const requestUrl = new URL(event.request.url);
+  const requestPath = requestUrl.pathname;
+  const fileName = requestPath.substring(requestPath.lastIndexOf("/") + 1);
+
+  if (requestPath === latestsPath || fileName === "sw.js") {
+    // Network Only strategy
+    // Simplemente dejamos pasar la petición sin hacer nada
+    return event.respondWith(fetch(event.request));
+  } else if (requestPath === imagePath) {
+    // Network First Then Offline strategy
+    return event.respondWith(networkFirstStrategy(event.request));
+  }
+});
+
+const networkFirstStrategy = async (request) => {
+  try {
+    return await fetchRequestAndCache(request);
+  } catch {
+    return await caches.match(request);
+  }
+};
+
+const fetchRequestAndCache = async (request) => {
+  const networkResponse = await fetch(request);
+  const clonedResponse = networkResponse.clone();
+  const cache = await caches.open(getCacheName(request));
+  cache.put(request, networkResponse);
+  return clonedResponse;
+};
+
+const getCacheName = (request) => {
+  const requestUrl = new URL(request.url);
+  const requestPath = requestUrl.pathname;
+
+  if (requestPath == imagePath) {
+    return carDealsCacheImagesName;
+  } else if (requestPath == carPath) {
+    return carDealsCachePagesName;
+  } else {
+    return carDealsCacheName;
+  }
+};
